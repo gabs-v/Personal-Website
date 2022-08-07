7b57ce3fda046121fa9daccf2893be6bdec294b7
gabs-v <gabyvazquez8297@gmail.com>
2022-08-07 14:21:59 -0700

    pictures

--- client/src/App.js
+++ client/src/App.js
@@ -27,6 +27,8 @@ function App() {
   return (
     <div className="App">
       <div className='container'>
+
+
         <div className='navbar'>
         <h2 className='navTitle'> Gaby Vazquez </h2>
         <ul>
@@ -37,6 +39,8 @@ function App() {
           ))}
         </ul>
         </div>
+
+
         <div className='mainPart'>
           <div className='section1'>
             <div>
@@ -44,35 +48,56 @@ function App() {
                 HEY NICE TO MEET YA, I'M GABY!<br/>
                 A FULL STACK DEVELOPER
               </div>
-              <p>
+              <p className='introAbout'>
               Recent software engineering graduate who's passionate about 
               <span> coding, problem-solving, and staying curious</span>
               </p>
             </div>
             <div>
-              <img className='contain' src="Order_gaby.png" alt="MDN Logo"/>
+              <img  src="Order_gaby.png" alt="MDN Logo"/>
             </div>
           </div>
+
+
           <div className='section2'>
-            <h2 className='sections-header' id='about'>
-              About Me!
+            <h2 className='sections-header'>
+              About Me! 
             </h2>
+            <p>
+              You might be asking yourself, "Well who exactly is this Gaby chick?"
+              Well allow me to introduce myself. <br/> I'm currently based in Las Vegas, Nevada
+              and a recent graduate of the software development course at 
+              Coding Dojo as of August 12th, 2022. Although I am just starting to break into tech,
+              I offer many things such as knowledge in full stack application in different  
+              languages and knowledge in algorithms. Not only am I passionate about this career 
+              field but I am both determined and disciplined.<br/> Above all else, 
+              I always offer my best and make an effort to keep growing my knowledge.
+            </p>
+            <img className='galary' src="Order_gaby.png" alt="MDN Logo"/>
           </div>
+
+
           <div className='section3'>
             <h2 className='sections-header' id='skills'>
               Skills!
             </h2>
           </div>
+
+
           <div className='section4'>
             <h2 className='sections-header' id='projects'>
               Past Projects!
             </h2>
           </div>
+
+
           <div className='section5'>
             <h2 className='sections-header' id='contact'>
               Contact Info!
             </h2>
           </div>
+          
+
         </div>
       </div>
     </div>
