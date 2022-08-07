c26ab5316d83ed4f9f157312b2105e3b9971423b
gabs-v <gabyvazquez8297@gmail.com>
2022-08-07 13:39:24 -0700

    stylin

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const navegationBar = [
+    {
+      id: 1,
+      title: "main"
+    },
+    {
+      id: 2,
+      title: "about"
+    },
+    {
+      id: 3,
+      title: "skills"
+    },
+    {
+      id: 4,
+      title: "projects"
+    },
+    {
+      id: 5,
+      title: "contact"
+    }
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+        <div className='navbar'>
+        <h2 className='navTitle'> Gaby Vazquez </h2>
+        <ul>
+          {navegationBar.map((navegation) => (
+            <li>
+              <a href={`#${navegation.title}`}> {navegation.title} </a>
+            </li> 
+          ))}
+        </ul>
+        </div>
+        <div className='mainPart'>
+          <div className='section1'>
+            <div>
+              <div className='intro'>
+                HEY NICE TO MEET YA, I'M GABY!<br/>
+                A FULL STACK DEVELOPER
+              </div>
+              <p>
+              Recent software engineering graduate who's passionate about 
+              <span> coding, problem-solving, and staying curious</span>
+              </p>
+            </div>
+            <div>
+              <img className='contain' src="Order_gaby.png" alt="MDN Logo"/>
+            </div>
+          </div>
+          <div className='section2'>
+            <h2 className='sections-header' id='about'>
+              About Me!
+            </h2>
+          </div>
+          <div className='section3'>
+            <h2 className='sections-header' id='skills'>
+              Skills!
+            </h2>
+          </div>
+          <div className='section4'>
+            <h2 className='sections-header' id='projects'>
+              Past Projects!
+            </h2>
+          </div>
+          <div className='section5'>
+            <h2 className='sections-header' id='contact'>
+              Contact Info!
+            </h2>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
